6e05117e31d5b2de1eeaf50a9fdc0af281978d95
mythri mutyala <mythrimutyala2006@gmail.com>
2026-01-09 15:12:33 +0530

    Create Node class and main method for testing
    Implemented Node class with data and next reference. Added main method to demonstrate Node functionality.

---
+++ Module2_Class/Nodeclass.java
@@ -0,0 +1,34 @@
+
+
+
+class Node{
+   private int data;
+    Node next;
+    Node(int a){
+        data=a;
+        next=null;
+        
+    }
+    public void getdata(){
+        System.out.println(data);
+    }
+    public void getref(){
+        System.out.println(next);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("mythri");
+        Node a =new Node(20);
+        
+        Node  b=new Node(28);
+        a.getdata();
+        a.getref();
+        
+        a.next=b;
+        
+        a.getref();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
