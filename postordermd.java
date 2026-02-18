b6f0ce2e2f086e0f9d2901c545ee196c72c4d33e
mythri mutyala <mythrimutyala2006@gmail.com>
2026-02-18 15:33:27 +0530

    Revise output for tree traversal methods
    Updated output format for tree traversal results.

--- postordermd.java
+++ postordermd.java
@@ -1,11 +1,8 @@
-output:
-
-Try programiz.pro
-
-post order traversal
-40
-20
-30
-10
-
+output
+Preorder:
+10 20 40 30 
+Inorder:
+40 20 10 30 
+Postorder:
+40 20 30 10 
 === Code Execution Successful ===
